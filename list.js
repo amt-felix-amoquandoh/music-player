0c640cf1e48659d42cc7fea3dcc78d3147a973e6
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-11-16 13:56:48 +0000

    Update list.js

--- list.js
+++ list.js
@@ -2,7 +2,98 @@ let musicFolder = [
     {
         name: "-Oyi-",
         artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
-        image: "",
-        source: ""
+        image: "./images/2baba.png",
+        source: "./songs/2Baba-Oyi-ft-HI-Idibia-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "Fake Love ",
+        artist:"Duncan Mighty ft Wizkid",
+        image: "./images/fakelove.png",
+        source: "./songs/Duncan\ Mighty\ –\ Fake\ Love\ ft\ Wizkid.mp3"
+    },
+    {
+        name: "Bad",
+        artist:"Kiss_Daniel",
+        image: "./images/music.jpg",
+        source: "./songs/Kizz-Daniel-Bad-feat-Wretch-32-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "Dzo",
+        artist:"Ko-Jo-Cue feat Worlasi",
+        image: "./images/dzo.jpg",
+        source: "./songs/Ko-Jo-Cue-Dzo-feat-Worlasi-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "Human_Being_",
+        artist:"MI_Abaga_Feat_2_Face_Sound_Sultan",
+        image: "./images/music3.jpg",
+        source: "./songs/MI_Abaga_Human_Being_Feat_2_Face_Sound_Sultan.mp3"
+    },
+    {
+        name: "Love-Anthem",
+        artist:"Obrafour-ft.-Trigmatic-x-A.I-–-",
+        image: "./images/music1.jpg",
+        source: "./songs/Obrafour-ft.-Trigmatic-x-A.I-–-Love-Anthem-Prod.by-JMJ-www.Ghanasongs.com_.mp3"
+    },
+    {
+        name: "By-You-",
+        artist:"-Simi--ft-Adekunle-Gold",
+        image: "./images/2baba.png",
+        source: "./songs/Simi-By-You-ft-Adekunle-Gold-\(TrendyBeatz.com\).mp3"
+    },
+    {
+        name: "Simi-Doyin-ft-Mr-Eazi",
+        artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
+        image: "./images/fakelove.png",
+        source: "./songs/Simi-Doyin-ft-Mr-Eazi.mp3"
+    },
+    {
+        name: "-Sobolo-",
+        artist:"-Stonebwoy",
+        image: "./images/ghsongs.png",
+        source: "./songs/Stonebwoy-Sobolo-www.Ghanasongs.com_.mp3"
+    },
+    {
+        name: "-Oyi-",
+        artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
+        image: "./images/2baba.png",
+        source: "./songs/2Baba-Oyi-ft-HI-Idibia-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "-Oyi-",
+        artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
+        image: "./images/2baba.png",
+        source: "./songs/2Baba-Oyi-ft-HI-Idibia-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "-Oyi-",
+        artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
+        image: "./images/2baba.png",
+        source: "./songs/2Baba-Oyi-ft-HI-Idibia-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "-Oyi-",
+        artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
+        image: "./images/2baba.png",
+        source: "./songs/2Baba-Oyi-ft-HI-Idibia-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "-Oyi-",
+        artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
+        image: "./images/2baba.png",
+        source: "./songs/2Baba-Oyi-ft-HI-Idibia-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "-Oyi-",
+        artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
+        image: "./images/2baba.png",
+        source: "./songs/2Baba-Oyi-ft-HI-Idibia-\(JustNaija.com\).mp3"
+    },
+    {
+        name: "-Oyi-",
+        artist:"2Baba-ft-HI-Idibia-(JustNaija.com)",
+        image: "./images/2baba.png",
+        source: "./songs/2Baba-Oyi-ft-HI-Idibia-\(JustNaija.com\).mp3"
     }
+
 ]
