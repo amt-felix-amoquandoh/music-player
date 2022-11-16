8593c38ec075b5faf776bfa48969ad90a02dd6e5
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-11-16 14:22:03 +0000

    Update music.js

---
+++ music.js
@@ -0,0 +1,10 @@
+const playerBox = document.querySelector(".container"),
+trackImage = playerBox.querySelector(".imageBox img"),
+musicTitle = playerBox.querySelector(".songDetails .songTitle"),
+artists = playerBox.querySelector(".songDetails .artist-e");
+
+let musicIndex = 1;
+
+window.addEventListener("load", () => {
+    
+})
